0b6140fba988292a397d958afb434d2e2f2c5168
lokesh-mali <mali.lokesh5050@gmail.com>
2022-10-28 10:19:47 +0530

    github and oauth

--- models/users.js
+++ models/users.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/t2s");
 const plm = require("passport-local-mongoose")
+const findOrCreate = require('mongoose-findorcreate')
 
 const userSchema = mongoose.Schema({
-  username:{type:String , required:true},
-  email:{type:String , required:true},
-  phoneNo:{type:String , required:true},
-  age:{type:String , required:true},
+  username:{type:String },
+  email:{type:String },
+  phoneNo:{type:String },
+  age:{type:String },
   role:{type:String , default:"user" , enum:['user' , 'admin']}
 })
 userSchema.plugin(plm , {usernameField:'email'});
+userSchema.plugin(findOrCreate)
 
 module.exports = mongoose.model("user" , userSchema)
--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,73 @@ const { allMessages } = require("../controllers/messagesController");
 const { isLoggedIn, checkIsLoggedIn } = require("../middlewares/middlewares");
 const { request } = require("express");
 
+//oauth google
+const GOOGLE_CLIENT_ID ="184644567601-712m204ljidhmliomk42a9deq45h7i4k.apps.googleusercontent.com";
+const GOOGLE_CLIENT_SECRET = "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";
+const findOrCreate = require("mongoose-findorcreate");
+
+var GoogleStrategy = require("passport-google-oauth2").Strategy;
+var GitHubStrategy = require("passport-github2").Strategy
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+      passReqToCallback: true,
+    },
+    function (request, accessToken, refreshToken, profile, done) {
+      // console.log(profile);
+      userModel.findOrCreate(
+        { email: profile.email, username: profile.given_name  },
+        function (err, user) {
+          console.log(user);
+          return done(err, user);
+        }
+      );
+    }
+  )
+);
+
+//oauth API
+router.get("/auth/google", passport.authenticate("google", { scope: ["email", "profile" ] })
+);
+
+router.get("/auth/google/callback", passport.authenticate("google", {
+    successRedirect: "/chats",
+    failureRedirect: "/",
+  })
+);
+
+
+
+//oauth github
+passport.use(new GitHubStrategy({
+  clientID: "9f40ffdbf3b47c015406",
+  clientSecret: "14bb1e39679168930595cba20a37f16da9505e7d",
+  callbackURL: "http://localhost:3000/auth/github/callback"
+},
+function(accessToken, refreshToken, profile, done) {
+  console.log(profile);
+  userModel.findOrCreate({ email: profile.id , username: profile.displayName.split("-")[0]+Math.floor(Math.random()*1000) },
+   function (err, user) {
+    return done(err, user);
+  });
+}
+));
+
+//oauth gihub API
+router.get('/auth/github', passport.authenticate('github', { scope: [ 'user:email' , "profile" ] }));
+
+router.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/chats');
+  });
+
+
+//Login Page API
 router.get("/", function (req, res, next) {
   res.render("login");
 });
