419515253abb661556f9ffdf9991193fdb9edede
lokesh-mali <mali.lokesh5050@gmail.com>
2022-11-21 00:08:27 +0530

    cod done

--- models/users.js
+++ models/users.js
@@ -1,13 +1,18 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/t2s");
+try {
+  mongoose.connect("mongodb://localhost/t2s");
+  
+} catch (error) {
+  console.log(error);
+}
 const plm = require("passport-local-mongoose")
 const findOrCreate = require('mongoose-findorcreate')
 
-const userSchema = mongoose.Schema({
-  username:{type:String },
-  email:{type:String },
-  phoneNo:{type:String },
-  age:{type:String },
+const userSchema =  mongoose.Schema({
+  username:String,
+  email:String ,
+  phoneNo:Number ,
+  age:Number ,
   role:{type:String , default:"user" , enum:['user' , 'admin']}
 })
 userSchema.plugin(plm , {usernameField:'email'});
